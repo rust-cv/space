28769ab29c09ac6649ad18b32ca14dcee851bbdb
Geordon Worley <vadixidav@gmail.com>
2018-10-19 13:26:39 -0400

    fixed some indentation issues and added a difference checker

--- src/octree.rs
+++ src/octree.rs
@@ -10,37 +10,36 @@ use nalgebra::Vector3;
 use num::{Float, FromPrimitive, ToPrimitive};
 
 pub trait Gatherer<Item, M> {
-        type Sum;
-        fn gather<'a, I>(&self, it: I) -> Self::Sum
-        where
-                Item: 'a,
-                I: Iterator<Item = (M, &'a Item)>;
+    type Sum;
+    fn gather<'a, I>(&self, it: I) -> Self::Sum
+    where
+        Item: 'a,
+        I: Iterator<Item = (M, &'a Item)>;
 }
 
 pub trait Folder {
-        type Sum;
-        fn sum<I>(&self, it: I) -> Option<Self::Sum>
-        where
-                I: Iterator<Item = Self::Sum>;
+    type Sum;
+    fn sum<I>(&self, it: I) -> Option<Self::Sum>
+    where
+        I: Iterator<Item = Self::Sum>;
 }
 
 pub struct LeveledRegion(pub i32);
 
 impl LeveledRegion {
-        pub fn discretize<S, M>(&self, point: Vector3<S>) -> Option<M>
-        where
-                S: Float + ToPrimitive + FromPrimitive + std::fmt::Debug + 'static,
-                M: Morton + std::fmt::Debug + 'static,
-        {
-                let bound = (S::one() + S::one()).powi(self.0);
-                if point.iter().any(|n| n.abs() > bound) {
-                        None
-                } else {
-                        // Convert the point into normalized space.
-                        let MortonWrapper(m) = (point
-                                .map(|n| (n + bound) / (S::one() + S::one()).powi(self.0 + 1)))
-                        .into();
-                        Some(m)
-                }
+    pub fn discretize<S, M>(&self, point: Vector3<S>) -> Option<M>
+    where
+        S: Float + ToPrimitive + FromPrimitive + std::fmt::Debug + 'static,
+        M: Morton + std::fmt::Debug + 'static,
+    {
+        let bound = (S::one() + S::one()).powi(self.0);
+        if point.iter().any(|n| n.abs() > bound) {
+            None
+        } else {
+            // Convert the point into normalized space.
+            let MortonWrapper(m) =
+                (point.map(|n| (n + bound) / (S::one() + S::one()).powi(self.0 + 1))).into();
+            Some(m)
         }
+    }
 }
--- src/octree/morton.rs
+++ src/octree/morton.rs
@@ -458,6 +458,23 @@ where
     }
 }
 
+/// Visits the values representing the difference, i.e. the keys that are in `primary` but not in `secondary`.
+pub fn region_map_difference<'a, T, U, M>(
+    primary: &'a MortonRegionMap<T, M>,
+    secondary: &'a MortonRegionMap<U, M>,
+) -> impl Iterator<Item = MortonRegion<M>> + 'a
+where
+    M: Morton,
+{
+    primary.keys().filter_map(move |&k| {
+        if secondary.get(&k).is_none() {
+            Some(k)
+        } else {
+            None
+        }
+    })
+}
+
 impl<S, M> From<Vector3<S>> for MortonWrapper<M>
 where
     M: Morton + std::fmt::Debug + 'static,
