e29dab9d780c74d80e52bcb48437940c9e4cb131
Geordon Worley <vadixidav@gmail.com>
2019-01-08 16:51:58 -0500

    add simulation that seems to gravitate towards the center and must be fixed

--- examples/barnes_hut.rs
+++ examples/barnes_hut.rs
@@ -5,13 +5,15 @@ use rand::{Rng, SeedableRng};
 use std::iter::repeat_with;
 use std::ops::{Add, AddAssign};
 
+use glium::*;
 use space::*;
 
-const POINTS: usize = 10000;
-const CACHE_SIZE: usize = 10000;
-const THETA2: f32 = 0.001;
-const EPS2: f64 = 1_000_000.0;
-const G: f64 = 1.0e-7;
+const POINTS: usize = 1000;
+const CACHE_SIZE: usize = 20000;
+const THETA2: f64 = 0.1;
+const EPS2: f64 = 100_000_000.0;
+const G: f64 = 1.0e12;
+const DRAG: f64 = 1.0e-1;
 
 struct Center;
 
@@ -53,30 +55,76 @@ where
 }
 
 // Width of the boxed region for a given depth.
-fn depth_width(depth: usize) -> f32 {
-    2.0f32.powi(-(depth as i32))
+fn depth_width(depth: usize) -> f64 {
+    2.0f64.powi(-(depth as i32))
+}
+
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+}
+
+implement_vertex!(Vertex, position);
+
+fn wrap_delta(pos: f64) -> f64 {
+    // Create shrunk_pos, which may still not be inside the space, but is within one stride of it
+    let shrunk_pos = pos % 1.0;
+
+    if shrunk_pos < -0.5 {
+        shrunk_pos + 1.0
+    } else if shrunk_pos > 0.5 {
+        shrunk_pos - 1.0
+    } else {
+        shrunk_pos
+    }
 }
 
 fn main() {
     let mut octree = octree_insertion::<u64, _>(random_points(POINTS));
     let mut rng = SmallRng::from_seed([1; 16]);
+
+    let mut events_loop = glutin::EventsLoop::new();
+    let window = glutin::WindowBuilder::new();
+    let context = glutin::ContextBuilder::new().with_vsync(true);
+    let display = glium::Display::new(window, context, &events_loop).unwrap();
+
+    let program = glium::Program::from_source(
+        &display,
+        "
+        #version 150 core
+        in vec2 position;
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+        ",
+        "
+        #version 150 core
+        void main() {
+            gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+        }
+        ",
+        None,
+    )
+    .unwrap();
+
     loop {
         // The cache needs to expire every iteration.
-        let (new_octree, _) = octree.iter().fold(
+        let (new_octree, _, verts) = octree.iter().fold(
             (
                 PointerOctree::new(),
                 MortonRegionCache::with_hasher(CACHE_SIZE, MortonBuildHasher::default()),
+                Vec::new(),
             ),
-            |(mut new_octree, cache), (m, old_vel)| {
-                let position: Vector3<f32> = MortonWrapper(m).into();
+            |(mut new_octree, cache, mut verts), (m, old_vel)| {
+                let position: Vector3<f64> = MortonWrapper(m).into();
                 let mut it = octree.iter_fold_random(
                     1,
                     move |region| {
                         // s/d is used in barnes hut simulations to control simulation accuracy.
                         // Here we are using it to control granularity based on screen space.
                         // We compute the square because it is more efficient.
-                        let region_location: Vector3<f32> = region.into();
-                        let distance2 = (region_location - position).norm_squared();
+                        let region_location: Vector3<f64> = region.into();
+                        let distance2 = (region_location - position).map(wrap_delta).norm_squared();
                         let width2 = depth_width(region.level).powi(2);
                         width2 > THETA2 * distance2
                     },
@@ -89,11 +137,13 @@ fn main() {
                 let v = m.decode().map(|n| n as f64);
 
                 // Compute the net inverse force without any scaling.
-                let acceleration = (&mut it).fold(Vector3::zeros(), |acc, (region, (n, pos))| {
+                let acceleration = (&mut it).fold(Vector3::zeros(), |acc, (_, (n, pos))| {
                     // Divide the position sum by `n` and subtract the current position so the result is `r'`.
                     // `n`, the number of particles, is our "mass". This is our delta vector.
                     // Because it can go negative in a dimension, its necessary to use signed.
                     let delta = pos.map(|n| n as f64) / f64::from(n) - v;
+                    // This wraps the delta so that it is toroidal.
+                    let delta = delta.map(wrap_delta);
                     // Now we need the dot product of this vector with itself. This produces `r^2`.
                     // The `EPS` is used to soften the interaction as if the two particles
                     // were a cluster of particles of radius `EPS`. It is squared in advance.
@@ -103,17 +153,50 @@ fn main() {
                     acc + delta * f64::from(n) / r3
                 });
 
-                // Scale the acceleration by `G`.
-                let acceleration = G * acceleration;
+                // Scale the acceleration by `G` and include drag.
+                let acceleration = G * acceleration - DRAG * old_vel;
 
                 // Take the midway between the old and new velocity and apply that to the position.
-                let new_position = (v + acceleration * 0.5 + old_vel)
-                    .map(|n| (n as i64 & ((1i64 << u64::dim_bits()) - 1) as i64) as u64);
-                new_octree.insert(u64::encode(new_position), old_vel + acceleration);
-                (new_octree, it.into())
+                let new_morton = u64::encode(
+                    (v + acceleration * 0.5 + old_vel)
+                        .map(|n| (n as i64 & ((1i64 << u64::dim_bits()) - 1)) as u64),
+                );
+                new_octree.insert(new_morton, old_vel + acceleration);
+                let position: Vector3<f32> = MortonWrapper(new_morton).into();
+                verts.push(Vertex {
+                    position: [position.x * 2.0 - 1.0, position.y * 2.0 - 1.0],
+                });
+                (new_octree, it.into(), verts)
             },
         );
 
+        // Replace the old octree.
         std::mem::replace(&mut octree, new_octree);
+
+        let vbo = glium::VertexBuffer::new(&display, &verts).unwrap();
+        let ibo = glium::index::NoIndices(glium::index::PrimitiveType::Points);
+
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 0.0, 0.0);
+        target
+            .draw(
+                &vbo,
+                &ibo,
+                &program,
+                &glium::uniforms::EmptyUniforms,
+                &Default::default(),
+            )
+            .unwrap();
+        target.finish().unwrap();
+
+        events_loop.poll_events(|event| {
+            if let glutin::Event::WindowEvent {
+                event: glutin::WindowEvent::CloseRequested,
+                ..
+            } = event
+            {
+                std::process::exit(0)
+            }
+        });
     }
 }
