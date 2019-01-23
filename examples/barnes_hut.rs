445ff440426ad92a09b4e834c6e584bc2f009eb8
Geordon Worley <vadixidav@gmail.com>
2019-01-23 14:13:21 -0500

    update barnes_hut example

--- examples/barnes_hut.rs
+++ examples/barnes_hut.rs
@@ -9,21 +9,21 @@ use glium::*;
 use space::*;
 
 const POINTS: usize = 1000;
-const CACHE_SIZE: usize = 20000;
-const THETA2: f64 = 0.02;
-const EPS2: f64 = 100_000_000.0;
-const G: f64 = 5.0e11;
-const DRAG: f64 = 2.0e-1;
+const CACHE_SIZE: usize = POINTS * 2;
+const THETA2: f32 = 0.01;
+const EPS2: f32 = 100_000_000.0;
+const G: f32 = 5.0e11;
+const DRAG: f32 = 3.0e-1;
 
 struct Center;
 
-impl<M> Folder<Vector3<f64>, M> for Center
+impl<M> Folder<Vector3<f32>, M> for Center
 where
     M: Morton + Add + AddAssign,
 {
     type Sum = (u32, Vector3<M>);
 
-    fn gather(&self, m: M, _: &Vector3<f64>) -> Self::Sum {
+    fn gather(&self, m: M, _: &Vector3<f32>) -> Self::Sum {
         (1, m.decode())
     }
 
@@ -37,7 +37,7 @@ where
     }
 }
 
-fn octree_insertion<M: Morton, I: Iterator<Item = M>>(points: I) -> PointerOctree<Vector3<f64>, M> {
+fn octree_insertion<M: Morton, I: Iterator<Item = M>>(points: I) -> PointerOctree<Vector3<f32>, M> {
     let mut octree = PointerOctree::new();
     octree.extend(points.map(|i| (i, Vector3::zeros())));
     octree
@@ -55,8 +55,8 @@ where
 }
 
 // Width of the boxed region for a given depth.
-fn depth_width(depth: usize) -> f64 {
-    2.0f64.powi(-(depth as i32))
+fn depth_width(depth: usize) -> f32 {
+    2.0f32.powi(-(depth as i32))
 }
 
 #[derive(Copy, Clone)]
@@ -66,9 +66,9 @@ struct Vertex {
 
 implement_vertex!(Vertex, position);
 
-fn wrap_delta(pos: f64) -> f64 {
+fn wrap_delta(pos: f32) -> f32 {
     // Bound must be positive
-    let bound = (1u64 << (u64::dim_bits() - 1)) as f64;
+    let bound = (1u64 << (u64::dim_bits() - 1)) as f32;
     let twobound = 2.0 * bound;
     // Create shrunk_pos, which may still not be inside the space, but is within one stride of it
     let shrunk_pos = pos % twobound;
@@ -119,18 +119,21 @@ fn main() {
                 Vec::new(),
             ),
             |(mut new_octree, cache, mut verts), (m, old_vel)| {
-                let position: Vector3<f64> = MortonWrapper(m).into();
+                let position: Vector3<f32> = MortonWrapper(m).into();
                 let position = position.map(|n| n + 0.5);
                 let mut it = octree.iter_fold_random(
                     21,
                     move |region| {
-                        // s/d is used in barnes hut simulations to control simulation accuracy.
-                        // Here we are using it to control granularity based on screen space.
-                        // We compute the square because it is more efficient.
-                        let region_location: Vector3<f64> = region.into();
-                        let distance2 = (region_location - position).map(wrap_delta).norm_squared();
-                        let width2 = depth_width(region.level).powi(2);
-                        width2 > THETA2 * distance2
+                        region.contains(m) || {
+                            // s/d is used in barnes hut simulations to control simulation accuracy.
+                            // Here we are using it to control granularity based on screen space.
+                            // We compute the square because it is more efficient.
+                            let region_location: Vector3<f32> = region.into();
+                            let distance2 =
+                                (region_location - position).map(wrap_delta).norm_squared();
+                            let width2 = depth_width(region.level).powi(2);
+                            width2 > THETA2 * distance2
+                        }
                     },
                     &Center,
                     &mut rng,
@@ -138,23 +141,23 @@ fn main() {
                 );
 
                 // Decode the morton into its int vector.
-                let v = m.decode().map(|n| n as f64 + 0.5);
+                let v = m.decode().map(|n| n as f32 + 0.5);
 
                 // Compute the net inverse force without any scaling.
                 let acceleration = (&mut it).fold(Vector3::zeros(), |acc, (_, (n, pos))| {
                     // Divide the position sum by `n` and subtract the current position so the result is `r'`.
                     // `n`, the number of particles, is our "mass". This is our delta vector.
                     // Because it can go negative in a dimension, its necessary to use signed.
-                    let delta = pos.map(|n| n as f64) / f64::from(n) - v;
+                    let delta = pos.map(|n| n as f32) / n as f32 - v;
                     // This wraps the delta so that it is toroidal.
                     let delta = delta.map(wrap_delta);
                     // Now we need the dot product of this vector with itself. This produces `r^2`.
                     // The `EPS` is used to soften the interaction as if the two particles
                     // were a cluster of particles of radius `EPS`. It is squared in advance.
-                    let r2 = delta.dot(&delta) as f64 + EPS2;
+                    let r2 = delta.dot(&delta) as f32 + EPS2;
                     let r3 = (r2 * r2 * r2).sqrt();
                     // We want `n * r' / r^3` as our final solution.
-                    acc + delta * f64::from(n) / r3
+                    acc + delta * n as f32 / r3
                 });
 
                 // Scale the acceleration by `G` and include drag.
